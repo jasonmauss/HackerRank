b50f39420e0edd69209e6e39458da1152f7881f7
Jason Mauss <jason.mauss@gmail.com>
2023-02-16 23:51:09 -0700

    initial commit

---
+++ DS_Arrays_SparseArrays/C#/app/Program.cs
@@ -0,0 +1,69 @@
+﻿// Solution for https://www.hackerrank.com/challenges/sparse-arrays/problem
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY stringList
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> stringList, List<string> queries)
+    {
+        return new List<int>();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        TextWriter textWriter = new StreamWriter(@"C:\Temp\Test.txt", true);
+
+        int stringListCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> stringList = new List<string>();
+
+        for (int i = 0; i < stringListCount; i++)
+        {
+            string stringListItem = Console.ReadLine();
+            stringList.Add(stringListItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(stringList, queries);
+
+        textWriter.WriteLine(String.Join("\n", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
